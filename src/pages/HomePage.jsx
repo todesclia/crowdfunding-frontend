508b8f6f9fbf9960743c4d37a7bfbfcf6bdbd257
lisa.todesco <todesco@gmail.com>
2024-11-19 18:14:29 +0800

    initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import { allProjects } from "../data";
+import ProjectCard from "../components/ProjectCard";
+import "./HomePage.css";
+
+function HomePage() {
+    return (
+       <div id="project-list">
+           {allProjects.map((projectData, key) => {
+               return <ProjectCard key={key} projectData={projectData} />;
+           })}
+       </div>
+   );
+  }
+  
+  export default HomePage;
